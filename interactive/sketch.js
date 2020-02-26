286b06327986b8fc0b2ed83b352cf9ab17887ff5
SilentSpins <51772266+SilentSpins@users.noreply.github.com>
2020-02-26 15:19:31 -0600

    interactive update

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -25,24 +25,27 @@ function setup() {
 function draw() {    
   rect(x, y, rectSize, rectSize);
   timer = round(random(1,3));
-  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+  if (frameCount % 60 === 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
     timer --;
   }
-  if (timer == 0) {
-    state = "green"
+  if (timer === 0) {
+    state = "green";
     fill(state);
     initialMilisecond = millis();
-    return initialMilisecond
+    return initialMilisecond;
   }
 }
 
 function mouseClicked() {
-   if (mouseX < 300 && mouseX > 0 && mouseY < 300 && mouseY > 0 && state === "green" || state === "blue"){
+  if (mouseX < 300 && mouseX > 0 && mouseY < 300 && mouseY > 0 && state === "green" || state === "blue"){
     fill("red");
     endMilisecond = millis();
-    results = ((round(initialMilisecond-endMilisecond)*-1))
-    text(results,windowWidth/2,y+50);
+    results = round(initialMilisecond-endMilisecond)*-1;
     state = "red";
-}
+  }
+  // eslint-disable-next-line use-isnan
+  if (results !== NaN){
+    print(results);
+  }
   
 }
