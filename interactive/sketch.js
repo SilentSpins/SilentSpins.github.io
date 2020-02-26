8d83eb407667c48f7b3b59cc3dc63d59ab44e8eb
SilentSpins <51772266+SilentSpins@users.noreply.github.com>
2020-02-26 14:43:33 -0600

    edited

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -1,64 +1,48 @@
-/* eslint-disable no-empty */
-// Reaction Time Game
-// Minh Chau
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-
 
 let x, y;
 let rectSize;
-let initialMilisecond = 0;
+let speed;
+let initialMilisecond;
 let endMilisecond;
-let stateColour;
-let timeToWait;
+let results;
+let state;
+let timer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   x = 0;
   y = 0;
   rectSize = 300;
-  stateColour = "blue";
+  speed = 5;
+  state = 0;
   strokeWeight(2);
   stroke(0);
-  fill("stateColour");
+  state = "blue";
+  fill("blue");
   background("grey");
 }
 
 function draw() {    
-  text(round(millis()) + " ms has elapsed.", windowWidth-150,y+25);
   rect(x, y, rectSize, rectSize);
-  function stateFinder();
-
-  if (stateColour === "red" && frameCount % 60 === 0 && timeToWait > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-    timeToWait --;
+  timer = round(random(1,3));
+  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+    timer --;
   }
-  if (timeToWait === 0) {
-    stateColour = green;
-    fill(stateColour);
+  if (timer == 0) {
+    state = "green"
+    fill(state);
     initialMilisecond = millis();
-    return initialMilisecond;
+    return initialMilisecond
   }
 }
 
 function mouseClicked() {
-  if (mouseX < 300 && mouseX > 0 && mouseY < 300 && mouseY > 0){
+   if (mouseX < 300 && mouseX > 0 && mouseY < 300 && mouseY > 0 && state === "green" || state === "blue"){
     fill("red");
     endMilisecond = millis();
-    print(round(initialMilisecond-endMilisecond)*-1);
-  }
+    results = ((round(initialMilisecond-endMilisecond)*-1))
+    text(results,windowWidth/2,y+50);
+    state = "red";
+}
   
-  function stateFinder(){
-    if (stateColour === "blue" && mouseIsPressed && mouseX < 300 && mouseX > 0 && mouseY < 300 && mouseY > 0){
-      stateColour = "red";
-      fill(stateColour);
-      timeToWait = random(1000,3000);
-    }
-    else if(stateColour === "red" && mouseIsPressed && mouseX < 300 && mouseX > 0 && mouseY < 300 && mouseY > 0){
-
-    }  
-  }
-
 }
